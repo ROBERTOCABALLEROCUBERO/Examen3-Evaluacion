bcb4dfc561712ee2c3889e3afd53cb752d6f4a0b
Roberto <robercaballero1999@gmail.com>
2022-05-20 20:45:24 +0200

    Terminado

--- ExamenEJ1_3EVA/src/main/java/Ejercicio3Exam/Roberto_regalarbombones.java
+++ ExamenEJ1_3EVA/src/main/java/Ejercicio3Exam/Roberto_regalarbombones.java
@@ -5,8 +5,11 @@
 package Ejercicio3Exam;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -19,20 +22,47 @@ public class Roberto_regalarbombones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HashMap<String, Integer> alumnos = new HashMap<>();
-         BufferedReader br = null;
-      
-      try {
-         
-         br =new BufferedReader(new FileReader("files/Libro1.csv"));
-         String line = br.readLine();
-         while (null!=line) {
-            String [] fields = line.split(SEPARATOR);
-         }
+        HashMap<String, Double> alumnos = new HashMap<>();
+        BufferedReader br = null;
+        String nombre;
+        int numalumnos, bombones, numlineas = 0;
+        Scanner leer = new Scanner(System.in);
+        //Declaro las variables
+        try {
+            System.out.println("¿Cuantos alumnos juegan?");
+            numalumnos = leer.nextInt();
+            System.out.println("¿Cual es el numero maximo de bombones que les pueden tocar?");
+            bombones = leer.nextInt();
+            //Leo el numero de alumnos
+            br = new BufferedReader(new FileReader("C:\\Users\\DAW\\Desktop\\tenerife\\participants.csv"));
+            String texto = null;
+            while ((texto = br.readLine()) != null && numlineas != numalumnos) {
+                String.valueOf(texto);
+                String[] dividir = texto.split("\"");
+                numlineas++;
+                if(dividir.length != 0){
+                alumnos.put(dividir[1], Math.random() * bombones);
+                }
+            }
+//Recorro todo el archi y llo meto en el hash map
+            System.out.println("Sobre que alumno quieres la información");
+            nombre = leer.next();
+
+            if (alumnos.containsKey(nombre)) {
+                System.out.printf("Los bombones de %s son %s", nombre, alumnos.get(nombre));
+            } else {
+                throw new Exception("No hay bombones para ti");
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+
+            System.out.println("No se ha encontrado el archivo");
+
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+
         }
-    
-    
 
     }
+
 }
