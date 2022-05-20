36938e4f2f59c54e7397012c185c0b40379f7c22
Roberto <robercaballero1999@gmail.com>
2022-05-20 18:57:43 +0200

    Ejercicio1Comentado

--- ExamenEJ1_3EVA/src/main/java/prog/roberto/examenej1_3eva/MeteorologiaMain.java
+++ ExamenEJ1_3EVA/src/main/java/prog/roberto/examenej1_3eva/MeteorologiaMain.java
@@ -18,79 +18,104 @@ public class MeteorologiaMain {
     /**
      * @param args the command line arguments
      */
+    //Lanza la excepcion FileNotFoundException
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-        String[] notas;
-        Integer suma_max = 0, suma_min = 0;
+        /*
+        
+        Declaro un array de Strings donde almacenare el resultado del split de cada linea que tiene la misma estructura.
+        La variable archivo será la que se tengo que introducir para que me lleve al fichero
+        Las variables suma_max y suma_min son para acumular y hacer luego la media de las temperaturas maximas y minimas
+        La fecha será el String donde almacenare la fecha con la mayor diferencia de temperatura entre maxima y minima
+        El resto de variables seran los datos leidos de cada linea, concretamente en la posicion 1 y 2 de mi array de Temperaturas.
+        Por ultimo un long para contar la lineas y ejecutar la media de temperaturas.
+         */
+
+        Double suma_max = 0.0, suma_min = 0.0;
         Scanner leer = null;
         String archivo;
-        String fecha;
+        String fecha = null;
         double diferencia_inicial = 0, temperatura_max_max = 0, temperatura_max_min = 0,
                 temperatura_min_max = 0, temperatura_min_min = 0;
         long numlineas = 0;
+//------------------------------------------------------------------------------------------------------------------------------------
         try {
+            Scanner leer2 = new Scanner(System.in);
             System.out.println("Introduce el nombre del archivo");
-            archivo = leer.next();
+            archivo = leer2.nextLine();
+            //Leo el archivo
             File ruta = new File("C:\\Users\\DAW\\Desktop\\tenerife\\" + archivo);
             leer = new Scanner(ruta);
+            //Lo introduzco dentro de un Scanner para recorrerlo.
             while (leer.hasNextLine()) {
+                //Cuento las lineas hasta que se acaben en el archivo
                 numlineas++;
                 String[] Temperaturas = leer.nextLine().split(" ");
-                ArrayList<Integer> nuevo = new ArrayList();
+                //Divido cada linea y lo almaceno constantemente en un array nuevo que es de Temperaturas.
 
-                for (int i = 1; i < Temperaturas.length; i++) {
+                suma_max = suma_max + Double.parseDouble(Temperaturas[1]);
+                suma_min = suma_min + Double.parseDouble(Temperaturas[2]);
+                //La posicion 1 sera de maximas y la 2 de minimas, ademas la primera es la fecha.
 
-                    suma_max = suma_max + Integer.parseInt(Temperaturas[1]);
-                    suma_min = suma_min + Integer.parseInt(Temperaturas[2]);
+                if (diferencia_inicial == 0) {
+                    diferencia_inicial = Double.parseDouble(Temperaturas[1]) - Double.parseDouble(Temperaturas[2]);
+                    //Inicializo la diferencia
+                }
+                if (diferencia_inicial <= Double.parseDouble(Temperaturas[1]) - Double.parseDouble(Temperaturas[2])) {
 
-                    if (diferencia_inicial == 0) {
-                        diferencia_inicial = Integer.parseInt(Temperaturas[1]) - Integer.parseInt(Temperaturas[2]);
-                    }
-                    if (diferencia_inicial < Integer.parseInt(Temperaturas[1]) - Integer.parseInt(Temperaturas[2])) {
-
-                        diferencia_inicial = Integer.parseInt(Temperaturas[1]) - Integer.parseInt(Temperaturas[2]);
-                        fecha = Temperaturas[0];
-
-                    }
-                    if (temperatura_max_max == 0) {
-                        temperatura_max_max = Integer.parseInt(Temperaturas[1]);
-                        temperatura_max_min = Integer.parseInt(Temperaturas[1]);
-                        temperatura_min_max = Integer.parseInt(Temperaturas[2]);
-                        temperatura_min_min = Integer.parseInt(Temperaturas[2]);
-                    }
-
-                    if (temperatura_max_max < Integer.parseInt(Temperaturas[1])) {
-                        temperatura_max_max = Integer.parseInt(Temperaturas[1]);
-                    } else {
-                        if (temperatura_max_min > Integer.parseInt(Temperaturas[1])) {
-                            temperatura_max_min = Integer.parseInt(Temperaturas[1]);
-                        }
-                    }
-                    if (temperatura_min_max < Integer.parseInt(Temperaturas[2])) {
-                        temperatura_min_max = Integer.parseInt(Temperaturas[2]);
-                    } else {
-                        if (temperatura_min_min > Integer.parseInt(Temperaturas[1])) {
-                            temperatura_max_min = Integer.parseInt(Temperaturas[1]);
-                        }
-                    }
+                    diferencia_inicial = Double.parseDouble(Temperaturas[1]) - Double.parseDouble(Temperaturas[2]);
+                    fecha = Temperaturas[0];
+                    //Asigno la fecha, ademas de comparar las diferencias para quedarme con la maxima.
 
                 }
+                if (temperatura_max_max == 0) {
+                    temperatura_max_max = Double.parseDouble(Temperaturas[1]);
+                    temperatura_max_min = Double.parseDouble(Temperaturas[1]);
+                    temperatura_min_max = Double.parseDouble(Temperaturas[2]);
+                    temperatura_min_min = Double.parseDouble(Temperaturas[2]);
+                    //Inicializo todas las temperaturas maximas y minimas.
+                }
+
+                if (temperatura_max_max < Double.parseDouble(Temperaturas[1])) {
+                    temperatura_max_max = Double.parseDouble(Temperaturas[1]);
+                } else {
+                    if (temperatura_max_min > Double.parseDouble(Temperaturas[1])) {
+                        temperatura_max_min = Double.parseDouble(Temperaturas[1]);
+                    }
+                }
+                //Comparo para quedarme con la maxima mas alta y con la minima mas baja.
+
+                if (temperatura_min_max < Double.parseDouble(Temperaturas[2])) {
+                    temperatura_min_max = Double.parseDouble(Temperaturas[2]);
+                } else {
+                    if (temperatura_min_min > Double.parseDouble(Temperaturas[1])) {
+                        temperatura_max_min = Double.parseDouble(Temperaturas[1]);
+                    }
+                }
+                //Comparo para quedarme con la minima mas alta y con la minima mas baja.
 
             }
-            System.out.printf("Temperatura maxima mas alta y mas baja: %s     %s", temperatura_max_max, temperatura_max_min);
-            System.out.printf("Temperatura minima mas alta y mas baja: %s     %s", temperatura_min_max, temperatura_min_min);
-            System.out.printf("Temperatura maxima media: %s    ", suma_max / numlineas);
-            System.out.printf("Temperatura minima media: %s    ", suma_min / numlineas);
-            System.out.printf("Temperatura intermedia: %s    ", (((suma_max / numlineas) + (suma_min / numlineas)) / 2));
-            System.out.printf("Mayor diferencia de temperatura: %s %s");
-            
-
+            System.out.println("Temperatura maxima mas alta y mas baja:  " + temperatura_max_max + " " + temperatura_max_min);
+            System.out.println("Temperatura minima mas alta y mas baja:  " + temperatura_min_max + " " + temperatura_min_min);
+            System.out.println("Temperatura maxima media: " + suma_max / numlineas);
+            System.out.println("Temperatura minima media: " + suma_min / numlineas);
+            System.out.println("Temperatura intermedia: " + (((suma_max / numlineas) + (suma_min / numlineas)) / 2));
+            System.out.println("Mayor diferencia de temperatura: " + diferencia_inicial + "\tEn la fecha: " + fecha);
+//Lo imprimo todo
             leer.close();
+            //cierro el Scanner.
+
         } catch (FileNotFoundException a) {
 
             System.out.println("Archivo no encontrado");
 
+        } catch (NumberFormatException e) {
+
+            System.out.println("El numero que se ha registrado no es decimal");
+        } catch (Exception b) {
+            System.out.println("Ha ocurrido un error");
         }
+        //Controlo las excepciones
     }
 
 }
